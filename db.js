fa25c34a1f27af24024a41ba4281fb733493d373
Danny Rojas <danny.rojas@gco.com.co>
2021-07-06 16:01:37 -0500

    correction

--- db.js
+++ db.js
@@ -1,16 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
-
 let db;
-
 const loadDB = async () => {
     if (db) {
         return db;
     }
-    try {
+    try{
         const client = await MongoClient.connect(process.env.DB_HOST, {useNewUrlParser: true, useUnifiedTopology: true});
         db = client.db(process.env.DB_NAME);
-    } catch (err) {
-        Raven.captureException(err);
+    }catch(err){
+        throw err;
     }
     return db;
 };
